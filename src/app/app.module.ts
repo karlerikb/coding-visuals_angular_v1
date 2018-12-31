e5ad72f48645a460ee349300abcaf000e747a7df
Karl-Erik Borkmann <karlerikb@users.noreply.github.com>
2018-12-31 14:26:37 +0200

    added initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CoursesComponent } from './courses/courses.component';
+import { NotesComponent } from './notes/notes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    CoursesComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule
